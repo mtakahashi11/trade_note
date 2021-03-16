87d7bb5771fb46d899c65d7fe61ed0d9f31be56a
mtakahashi11 <76547704+mtakahashi11@users.noreply.github.com>
2021-03-16 20:56:44 +0900

    投稿詳細画面の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index ,:show]
+  before_action :set_post, only: [:show]
   
   def index
     @posts = Post.includes(:user).order('created_at DESC')
@@ -18,8 +19,15 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
   def post_params
     params.require(:post).permit(:description, :image).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
