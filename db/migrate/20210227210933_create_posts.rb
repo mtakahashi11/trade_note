4a7b675ab25f289d1f1df5832f410ba61c8ca3d2
mtakahashi11 <76547704+mtakahashi11@users.noreply.github.com>
2021-03-16 15:20:37 +0900

    新規投稿機能の実装

--- db/migrate/20210227210933_create_posts.rb
+++ db/migrate/20210227210933_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :description
-      t.text :image
+      t.text     :description, null: false
+      t.references :user,        foreign_key: true
       t.timestamps
     end
   end
